9d2a3fbd5e34d37cd4e2fc32b1ed26b181f32a2c
Zeeshan Shaikh <zeeshan6362034446@gmail.com>
2026-07-19 16:34:37 +0530

    Update lambda_function.py

---
+++ Assignment-1-S3-Cleanup/lambda_function.py
@@ -0,0 +1,39 @@
+import boto3
+from datetime import datetime, timezone, timedelta
+
+s3 = boto3.client("s3")
+
+BUCKET_NAME = "YOUR_BUCKET_NAME"
+
+AGE_MINUTES = 30
+
+def lambda_handler(event, context):
+
+    cutoff_time = datetime.now(timezone.utc) - timedelta(days=AGE_DAYS)
+
+    paginator = s3.get_paginator("list_objects_v2")
+
+    deleted = []
+
+    for page in paginator.paginate(Bucket=BUCKET_NAME):
+
+        if "Contents" not in page:
+            continue
+
+        for obj in page["Contents"]:
+
+            if obj["LastModified"] < cutoff_time:
+
+                s3.delete_object(
+                    Bucket=BUCKET_NAME,
+                    Key=obj["Key"]
+                )
+
+                print(f"Deleted: {obj['Key']}")
+
+                deleted.append(obj["Key"])
+
+    return {
+        "statusCode": 200,
+        "deleted_objects": deleted
+    }
